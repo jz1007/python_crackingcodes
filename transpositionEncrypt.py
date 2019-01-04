3cbe128ae9c35cc9a2b557eff3a8c3ec557d5d78
numberone107 <jzavilla+code@alumni.nd.edu>
2019-01-03 21:26:11 -0500

    Add files via upload

---
+++ transpositionEncrypt.py
@@ -0,0 +1,22 @@
+#https://www.nostarch.com/crackingcodes/ (BSD License)
+
+import pyperclip
+
+def main():
+    myMessage = input('Message to encrypt: ')
+    myKey = input('Key: ')
+    ciphertext = encryptMessage(myKey, myMessage)
+    print(ciphertext + '|')
+    pyperclip.copy(ciphertext)
+
+def encryptMessage(key,message):
+    ciphertext = [''] * int(key)
+    for column in range(int(key)):
+        currentIndex = column
+        while currentIndex < len(message):
+            ciphertext[column] += message[currentIndex]
+            currentIndex += int(key)
+    return ''.join(ciphertext)
+
+if __name__ == '__main__':
+    main()
